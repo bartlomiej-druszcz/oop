943efee2640835fdb597e3346edfa6dd9672592c
Bartłomiej Druszcz <bartlomiej.druszcz@gmail.com>
2022-09-24 11:04:46 +0200

    toString/equals/hashCode

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,19 @@
 package org.example;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
+        Person person1 = new Person("Jan", "Nowak", 45, "male");
+        Person person2 = new Person("Alina", "Kowalska", 88, "female");
+        Person person3 = new Person("Janusz", "Nowy", 68, "male");
 
-        System.out.println("Hello World!"
-        );
+        person1.fullName();
+        System.out.println("Czy na emeryturze? " + person1.isRetired());
+        person2.fullName();
+        System.out.println("Czy na emeryturze? " + person2.isRetired());
+        person3.fullName();
+        System.out.println("Czy na emeryturze? " + person3.isRetired());
+
+        System.out.println(person1);
+        System.out.println(person1.equals(person2));
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -4,22 +4,31 @@ package org.example;
  * Napisz klasę Box, które będzie przechowywać elementy dowolnego typu, np. int, String
  */
 
+import java.util.Objects;
+
 /**
  * 1) Napisz klasę Person
- *
+ * <p>
  * Z polami:
- *
+ * <p>
  * - firstName    ->     STRING
  * - lastName     ->     STRING
  * - age          ->     NUMBER
  * - gender       ->     STRING
- *
+ * <p>
  * 2) Dopisz construktor:
- *
+ * <p>
  * - bezparametrowy
  * - dla wszystkich parametrów
- *
+ * <p>
  * 3) Dopisz settery i gettery dla wszystkich pól.
+ * <p>
+ * 4) dopisz metode fullName()
+ * Dopisz metodę public boolean isRetired() sprawdzającą czy osoba jest emerytem (kobieta powyżej 65 lat, mężczyzna powyżej 67)
+ * <p>
+ * Przeładuj metodę toString() aby wyświetlić wszystkie informacje o danej osobie.
+ *
+ * Dodaj metody .equals() and .hashcode()
  */
 
 public class Person {
@@ -69,5 +78,36 @@ public class Person {
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    public void fullName() {
+        System.out.println(firstName + " " + lastName);
+    }
+
+    public boolean isRetired() {
+        if ("female".equalsIgnoreCase(gender)) {//zabezpieczenie przed null
+            return age > 65;
+        } else if ("male".equalsIgnoreCase(gender)) {
+            return age > 67;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "FirstName = " + firstName + ", LastName = " + lastName + ", Age = " + age + ", Gender = " + gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age, gender);
     }
 }
